60cdf769b2ab723dcceadd12d3aed693d225b05c
jugal-chanda <jugalchanda7@gmail.com>
2020-11-16 13:05:47 +0600

    Upload vedio search

--- vedios/views.py
+++ vedios/views.py
@@ -34,12 +34,15 @@ def vedio_links_students(request,id):
     subject = Subject.objects.get(pk=id)
     classlinks = subject.classlink_set.all().order_by('classdate')
     vedios = []
-    for classlink in classlinks:
-        vedio = classlink.vedio_set.all().first()
-        if vedio:
-            vedio_id = vedio.url.split('=')[1]
-            vedios.append({'title': vedio.title,'vedio_id': vedio_id,'classdate':classlink.classdate})
-            context['vedios'] = vedios
+    if request.POST:
+        pass
+    else:        
+        for classlink in classlinks:
+            vedio = classlink.vedio_set.all().first()
+            if vedio:
+                vedio_id = vedio.url.split('=')[1]
+                vedios.append({'title': vedio.title,'vedio_id': vedio_id,'classdate':classlink.classdate})
+                context['vedios'] = vedios
     return render(request,'students/vedios.html',context)
 
 def vedio_links_teachers(request,id):
